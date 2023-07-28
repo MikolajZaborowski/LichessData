8d57143044069397fed0dd2ac99a56ba2c2f577c
Mikolaj_Zaborowski <evilsaintpl9@gmail.com>
2023-07-28 14:35:10 +0200

    Main notebook

--- project/data_operations/json_sample_operations.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def read_json(path):
-    with open(path, "r") as f:
-        return json.load(f)
-
-def write_json(path, data):
-    with open(path, "w") as f:
-        f.dump(data, f)
